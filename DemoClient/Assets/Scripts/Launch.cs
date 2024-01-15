c391daed53dec484f21e112dc6e2f6f70f2297fe
linkh <421698843@qq.com>
2024-01-16 00:18:25 +0800

    add sdk

---
+++ DemoClient/Assets/Scripts/Launch.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Launch : MonoBehaviour
+{
+    InputField IPInputField;
+    InputField PortInputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IPInputField = transform.Find("IPInputField").GetComponent<InputField>();
+        PortInputField = transform.Find("PortInputField").GetComponent<InputField>();
+        if (IPInputField.text.Length == 0)
+        {
+            IPInputField.text = "127.0.0.1";
+        }
+
+        if (PortInputField.text.Length == 0)
+        {
+            PortInputField.text = 12345.ToString();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
